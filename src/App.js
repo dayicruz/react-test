a569c87d3b5015f44c84a6633cd6923d00184a5e
asmatar <deruelle.arthur@gmail.com>
2023-09-06 18:39:55 +0200

    commit 2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
      test react commit 1
+     <div className="dayi">jydgcil</div>
       </header>
     </div>
   );
